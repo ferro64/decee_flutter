018ebcf2195244dac18a144755d1dd7080dfc066
ferro64 <rauta64@gmail.com>
2021-12-15 17:49:06 +0100

    copy projet to repo

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: const DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatelessWidget {
+  DicePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var leftDiceNumber = 1;
+
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextButton(
+                onPressed: () {
+                  print('Left button got pressed!');
+                },
+                child: Image.asset('images/dice$leftDiceNumber.png'),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextButton(
+                onPressed: () {
+                  print('Right button got pressed!');
+                },
+                child: Image.asset('images/dice1.png'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
